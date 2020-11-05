0d57fc558a1f1de03189f6a223dc12c80ffe0611
Tim Lianov <t.lianov@firmshift.com>
2020-11-05 11:09:40 +0300

    fix

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -84,7 +84,7 @@ async function main() {
     await deployGovernor(governanceRouter);
     await deployRegister(governanceRouter);
     try {
-      let file = new File(JSON.stringify(ADDR), 'contract-addresses.json',  { type: 'text/plain' })
+      let file = new File(ADDR, 'contract-addresses.json',  { type: 'text/plain' })
     } catch {
       console.log('Failed to create addresses file')
     }
